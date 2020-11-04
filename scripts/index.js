f4bbc0b662cdfac33990093e43f95a2b939ea157
chadwyck242 <chadwyck.codeart@gmail.com>
2020-11-03 21:01:57 -0700

    Add Google Maps API Javascript functionality, and initial Hakata map location and marker

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
 // Create the script tag, set the appropriate attributes
 var script = document.createElement("script");
 script.src =
-  "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap";
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEcObo8RX6RLGR8IABulqIwhjnGcr-kOg&callback=initMap";
 script.defer = true;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
   // JS API is loaded and available
+  const hakataPortTower = { lat: 33.604, lng: 130.3978 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: hakataPortTower,
+  });
+  new google.maps.Marker({
+    position: hakataPortTower,
+    map,
+    title: "Hello Hakata!",
+  });
 };
 
 // Append the 'script' element to 'head'
