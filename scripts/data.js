9794c018e3f9f26a49c110dde11f84ca31903834
chadwyck242 <chadwyck.codeart@gmail.com>
2020-11-13 19:40:45 -0700

    Add Javascript functionality to display Google Map and initial Markers

---
+++ scripts/data.js
@@ -0,0 +1,8 @@
+const data = [
+  {
+    name: "Haka Port Tower",
+    location: { lat: 33.8, lng: 130.41 },
+  },
+];
+
+export default { data };
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,47 @@
 // Create the script tag, set the appropriate attributes
 var script = document.createElement("script");
 script.src =
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEcObo8RX6RLGR8IABulqIwhjnGcr-kOg&callback=initMap";
+  "https://maps.googleapis.com/maps/api/js?v=quarterly&key=&callback=initMap&libraries=places&v=quarterly";
 script.defer = true;
+
+let map, marker;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
   // JS API is loaded and available
   const hakataPortTower = { lat: 33.604, lng: 130.3978 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
     center: hakataPortTower,
   });
-  new google.maps.Marker({
-    position: hakataPortTower,
-    map,
-    title: "Hello Hakata!",
-  });
+
+  const data = [
+    {
+      name: "Haka Port Tower",
+      position: new google.maps.LatLng(33.604, 130.3978),
+    },
+    {
+      name: "Hakata Station",
+      position: new google.maps.LatLng(33.5897, 130.4207),
+    },
+  ];
+
+  for (let i = 0; i < data.length; i++) {
+    marker = new google.maps.Marker({
+      position: data[i].position,
+      title: data[i].name,
+      map: map,
+    });
+    marker.addListener("click", toggleBounce);
+  }
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
 };
 
 // Append the 'script' element to 'head'
