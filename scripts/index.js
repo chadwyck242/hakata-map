b38b2c94a4cc12c79c9487fe9af87b213e093669
chadwyck242 <chadwyck.codeart@gmail.com>
2020-11-21 12:45:54 -0700

    Change marker labels to be a part of the array of data objects

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,6 @@ script.defer = true;
 
 var map;
 var markers = [];
-const labels = "ABCDEFGHIJKLMNOPQRSTUVWKYZ";
-let labelIndex = 0;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
@@ -22,66 +20,82 @@ window.initMap = function () {
     {
       name: "Yakiniku Champion",
       position: new google.maps.LatLng(33.589813, 130.420062),
+      label: "A",
     },
     {
       name: "Yakiniku no Bakuro Hakata-ten",
       position: new google.maps.LatLng(33.589438, 130.412312),
+      label: "B",
     },
     {
       name: "Kuroichi Yakiniku",
       position: new google.maps.LatLng(33.592938, 130.401937),
+      label: "C",
     },
     {
       name: "Ryuen Nishinakasu shop",
       position: new google.maps.LatLng(33.590812, 130.406313),
+      label: "D",
     },
     {
       name: "カルビ亭福岡 黒毛和牛専門店",
       position: new google.maps.LatLng(33.583562, 130.402938),
+      label: "E",
     },
     {
       name: "Grilled meat Hachihachi Hakata shop",
       position: new google.maps.LatLng(33.586062, 130.427563),
+      label: "F",
     },
     {
       name: "牛仙人",
       position: new google.maps.LatLng(33.588313, 130.407562),
+      label: "G",
     },
     {
       name: "福岡 焼肉コウシ",
       position: new google.maps.LatLng(33.586687, 130.403813),
+      label: "H",
     },
     {
       name: "酒肴焼肉 彩稟 さいりん",
       position: new google.maps.LatLng(33.586813, 130.425187),
+      label: "I",
     },
     {
       name: "スタミナ家どろんぱっ",
       position: new google.maps.LatLng(33.586687, 130.431938),
+      label: "J",
     },
     {
       name: "カウンター焼肉ジャイアン家",
       position: new google.maps.LatLng(33.604312, 130.414688),
+      label: "K",
     },
     {
       name: "焼肉きらくや",
       position: new google.maps.LatLng(33.610563, 130.441688),
+      label: "L",
     },
     {
       name: "焼肉黄華園",
       position: new google.maps.LatLng(33.581187, 130.421938),
+      label: "M",
     },
     {
       name: "赤身焼肉 USHIO（ウシオ)",
       position: new google.maps.LatLng(33.582687, 130.422188),
+      label: "N",
     },
     {
       name: "焼肉 KAN",
       position: new google.maps.LatLng(33.609687, 130.451937),
+      label: "O",
     },
     {
       name: "焼肉 明秀苑",
       position: new google.maps.LatLng(33.599563, 130.466687),
+      label: "P",
     },
   ];
 
@@ -89,17 +103,17 @@ window.initMap = function () {
     clearMarkers();
 
     for (let i = 0; i < data.length; i++) {
-      addMarkers(data[i].position, data[i].name, i * 200);
+      addMarkers(data[i].position, data[i].name, data[i].label, i * 200);
     }
   }
 
-  function addMarkers(position, title, timeout) {
+  function addMarkers(position, title, label, timeout) {
     window.setTimeout(() => {
       markers.push(
         new google.maps.Marker({
           position: position,
           animation: google.maps.Animation.DROP,
-          label: labels[labelIndex++ % labels.length],
+          label: label,
           title: title,
           map: map,
         })
